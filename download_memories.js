70e89a204bd4f4e85e504136f19fc98c8df8d2f8
Grey Ruessler <greyruessler@gmail.com>
2021-11-14 00:05:52 -0600

    working downloads

--- download_memories.js
+++ download_memories.js
@@ -1,5 +1,38 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+const fs = require("fs");
+const request = require('request');
+
+function downloadMemories(url, name) {
+	const parts = url.split("?");
+
+    const baseURL = parts[0];
+
+    const params = parts[1];
+    const form = JSON.parse('{"' + decodeURI(params).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+    
+    const options = {
+        uri: baseURL,
+        method: 'POST',
+        headers: {
+        },
+        form: form
+    };
+    
+    request.post(options, function(err, res, success){
+        if (!success) {
+            console.log(err);
+            return;
+        }
+        var fetch = request(success);
+        fetch.on('response',  function (res) {
+            const queryIndex = success.indexOf('?');
+            const dotIndex = success.lastIndexOf('.',queryIndex);
+            const ext = success.substring(dotIndex, queryIndex);
+            res.pipe(fs.createWriteStream('./' + name + ext));
+        });
+    });
+}
 
 const domProm = JSDOM.fromFile('memories_history.html', {
     resources: "usable",
@@ -34,5 +67,11 @@ domProm.then((dom) => {
                 + ' - ' + date.toLocaleTimeString().replaceAll(':','.')
             });
     });
-    console.log(memories);
+    for (let i=0; i<5; i++) {
+        const memory = memories[i];
+        const url = memory.url
+                          .replace('javascript:downloadMemories(\'', '')
+                          .replace('\');','');
+        setTimeout(() => downloadMemories(url,'output/' + memory.date), i*250);
+    }
 });
